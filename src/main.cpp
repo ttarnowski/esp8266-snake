6f25fb647b47070701598b1c5311283458e91463
Tomasz Tarnowski <tomasz.tarnowski@thirdbridge.com>
2021-06-23 12:41:35 +0100

    Day48: Snake movement logic.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 #define RIGHT_BUTTON D2
 #define UP_BUTTON D4
 
+#define WIDTH 160
+#define HEIGHT 128
+
 Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST);
 
 struct Point {
@@ -20,7 +23,44 @@ struct Point {
   uint8_t y;
 };
 
-Point p;
+struct Game {
+  Game(Snake &s) : snake_(s) {}
+
+  enum Pixel { Empty = 0, Up = 1, Left = 2, Right = 3, Down = 4, Head = 5 };
+
+  void move_snake() {}
+
+private:
+  Snake &snake_;
+  uint8_t field[WIDTH][HEIGHT];
+};
+
+struct Snake {
+  friend Game;
+  enum Direction { Up = 1, Left = 2, Right = 3, Down = 4 };
+
+  Snake(uint8_t w, uint8_t h) {
+    head_.x = w / 2 + 3;
+    tail_.x = w / 2 - 3;
+
+    tail_.y = head_.y = h / 2;
+
+    direction_ = Right;
+  }
+
+  void set_direction(Direction d) { direction_ = d; }
+
+  const Point &get_head() { return head_; }
+
+  const Point &get_tail() { return tail_; }
+
+private:
+  Point head_;
+  Point tail_;
+  Direction direction_;
+};
+
+Snake snake(WIDTH, HEIGHT);
 
 void setup() {
   Serial.begin(115200);
@@ -34,31 +74,22 @@ void setup() {
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST77XX_BLACK);
   tft.setRotation(3);
-
-  p.x = 80;
-  p.y = 64;
 }
 
 bool is_button_pressed(uint8_t pin) { return digitalRead(pin) == LOW; }
 
 void loop() {
   if (is_button_pressed(UP_BUTTON)) {
-    p.y--;
   }
 
   if (is_button_pressed(DOWN_BUTTON)) {
-    p.y++;
   }
 
   if (is_button_pressed(LEFT_BUTTON)) {
-    p.x--;
   }
 
   if (is_button_pressed(RIGHT_BUTTON)) {
-    p.x++;
   }
 
   delay(100);
-
-  tft.drawPixel(p.x, p.y, ST7735_WHITE);
 }
